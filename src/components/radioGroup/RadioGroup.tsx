3c2d6f42376b010cb4e523d83b3bcf432deca665
Yeonnj <duswo9595@naver.com>
2025-06-04 18:01:37 +0900

    fix: radioGroup 불필요한 타입정리

--- src/components/radioGroup/RadioGroup.tsx
+++ src/components/radioGroup/RadioGroup.tsx
@@ -1,50 +1,43 @@
 import { createContext, forwardRef, useId, useMemo } from "react";
 import * as styles from "./RadioGroup.css";
 import clsx from "clsx";
-import { UseFormRegisterReturn } from "react-hook-form";
 
 interface RadioGroupProps
   extends Omit<React.ComponentPropsWithoutRef<"fieldset">, "onChange"> {
-  value?: string | number;
-  required?: boolean;
-  defaultValue?: string | number;
   children: React.ReactNode;
+  value?: string | number;
+  defaultValue?: string | number;
   label?: React.ReactNode;
-  register?: UseFormRegisterReturn;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const RadioGroupContext = createContext<Pick<
   RadioGroupProps,
-  "name" | "value" | "onChange" | "defaultValue" | "register"
+  "name" | "value" | "onChange" | "defaultValue"
 > | null>(null);
 
 const RadioGroup = forwardRef<HTMLFieldSetElement, RadioGroupProps>(
-  (
-    { label, value, defaultValue, children, onChange, register, ...props },
-    ref
-  ) => {
+  ({ label, value, defaultValue, children, onChange, ...props }, ref) => {
     const uid = useId();
     const nameToUse = props?.name ?? uid;
 
     const contextValue = useMemo(() => {
       return {
-        value: value,
-        defaultValue: defaultValue,
-        register,
-        name: nameToUse,
+        value,
+        defaultValue,
         onChange,
+        name: nameToUse,
       };
-    }, [nameToUse, register, onChange, value, defaultValue]);
+    }, [nameToUse, onChange, value, defaultValue]);
 
     return (
       <fieldset
         {...props}
         ref={ref}
-        className={clsx(styles.wrapper, props.className)}
+        className={clsx(styles.radioGroup, props.className)}
       >
+        <legend className={styles.radioGroupLegend}>{label}</legend>
         <RadioGroupContext.Provider value={contextValue}>
-          <legend className={styles.inputLabel}>{label}</legend>
           {children}
         </RadioGroupContext.Provider>
       </fieldset>
