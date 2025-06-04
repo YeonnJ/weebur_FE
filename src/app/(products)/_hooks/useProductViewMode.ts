236294fd663fbd2219d730f12bbed75c55b88c02
Yeonnj <duswo9595@naver.com>
2025-06-04 19:21:51 +0900

    fix: style 수정작업

--- src/app/(products)/_hooks/useProductViewMode.ts
+++ src/app/(products)/_hooks/useProductViewMode.ts
@@ -1,42 +1,60 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getRandomViewMode } from "@/app/(products)/_utils/getRandomViewMode";
 import { ProductViewMode } from "@/app/(products)/_types";
 
+interface ViewModeInfo {
+  date: number;
+  viewMode: ProductViewMode;
+}
 const STORAGE_KEY = "productViewMode";
+const ONE_DAY_HOURS = 24;
+const ONE_HOUS_MS = 1000 * 60 * 60;
 
 export const useProductViewMode = (): ProductViewMode | null => {
   const [viewMode, setViewMode] = useState<ProductViewMode | null>(null);
 
-  useEffect(() => {
-    const storedViewMode = localStorage.getItem(STORAGE_KEY);
-    const now = new Date();
+  const getDiffInHours = useCallback(
+    (parsedViewMode: ViewModeInfo, now: number) => {
+      const storedDate = new Date(parsedViewMode.date);
+      const diffInMs = now - storedDate.getTime();
+      return diffInMs / ONE_HOUS_MS;
+    },
+    []
+  );
 
-    if (storedViewMode) {
-      try {
-        const parsed = JSON.parse(storedViewMode);
-        const storedDate = new Date(parsed.date);
-        const diffInMs = now.getTime() - storedDate.getTime();
-        const diffInHours = diffInMs / (1000 * 60 * 60);
-
-        if (diffInHours < 24 && parsed.viewMode) {
-          setViewMode(parsed.viewMode);
-          return;
-        }
-      } catch (e) {
-        console.error("로컬스토리지 파싱 오류:", e);
-      }
-    }
-
+  const setViewModeInfo = useCallback((now: number) => {
     const newViewMode: ProductViewMode = getRandomViewMode();
-    setViewMode(newViewMode);
 
     const newData = {
       viewMode: newViewMode,
       date: now,
     };
 
+    setViewMode(newViewMode);
     localStorage.setItem(STORAGE_KEY, JSON.stringify(newData));
   }, []);
+
+  useEffect(() => {
+    const storedViewMode = localStorage.getItem(STORAGE_KEY) as ProductViewMode;
+    const now = new Date();
+
+    if (storedViewMode) {
+      try {
+        const parsedViewMode = JSON.parse(storedViewMode) as ViewModeInfo;
+        const diffInHours = getDiffInHours(parsedViewMode, now.getTime());
+
+        if (diffInHours < ONE_DAY_HOURS && parsedViewMode.viewMode) {
+          setViewMode(parsedViewMode.viewMode);
+          return;
+        }
+      } catch (e) {
+        // wip: 개선 사항으로 스낵바 추가
+        console.error("로컬스토리지 파싱 오류:", e);
+      }
+    }
+
+    setViewModeInfo(now.getTime());
+  }, [getDiffInHours, setViewModeInfo]);
 
   return viewMode;
 };
--- src/app/(products)/page.tsx
+++ src/app/(products)/page.tsx
@@ -3,10 +3,22 @@
 import React from "react";
 import ProductList from "./_components/productList/ProductList";
 import { useProductViewMode } from "@/app/(products)/_hooks/useProductViewMode";
+import Button from "@/components/button/Button";
+
+import * as styles from "./_components/layout/layout.css";
 
 const ProductsMainPage = () => {
   const viewMode = useProductViewMode();
 
-  return <>{viewMode !== null && <ProductList viewMode={viewMode} />}</>;
+  return (
+    <main className={styles.productsMainPageContainer}>
+      {viewMode !== null && (
+        <>
+          <Button.Link href={"/new"}>상품 등록하기</Button.Link>
+          <ProductList viewMode={viewMode} />
+        </>
+      )}
+    </main>
+  );
 };
 export default ProductsMainPage;
